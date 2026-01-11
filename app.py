8aecf7d65fc0bb212cc8810cac026679ed753930
Patito <emilizabaleta1412@gmail.com>
2026-01-11 10:46:39 -0400

    reywey

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def inicio():
     return render_template("index.html")
+
+@app.route("/prueba")
+def prueba():
+    return jsonify({"mensaje": "Hola"})
 
 if __name__ == "__main__":
     app.run(debug=True)
